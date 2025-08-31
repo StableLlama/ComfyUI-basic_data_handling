6266ab7b6a51a695590dac6d11b6ef9483421de1
StableLlama <stablellama@tutanota.com>
2025-08-31 17:00:35 +0200

    Change to use relative paths - trial to see whether this fixes the GitHub CI failing test cases

--- __init__.py
+++ __init__.py
@@ -6,7 +6,14 @@ __all__ = [
     "WEB_DIRECTORY",
 ]
 
-from .src.basic_data_handling import NODE_CLASS_MAPPINGS
-from .src.basic_data_handling import NODE_DISPLAY_NAME_MAPPINGS
+try:
+    # For ComfyUI
+    from .src.basic_data_handling import NODE_CLASS_MAPPINGS
+    from .src.basic_data_handling import NODE_DISPLAY_NAME_MAPPINGS
+except ImportError:
+    # For running tests
+    from src.basic_data_handling import NODE_CLASS_MAPPINGS
+    from src.basic_data_handling import NODE_DISPLAY_NAME_MAPPINGS
+
 
 WEB_DIRECTORY = "./web"
